4c638ba53233eeec060a29aca1af93ef5b7d73e0
Moiz Ali <amoiz4142@gmail.com>
2023-07-06 13:36:27 +0500

    Add searchable module with ES config

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -1,4 +1,10 @@
+require "elasticsearch"
 require "elasticsearch/model"
+
+Elasticsearch::Model.client = Elasticsearch::Client.new(
+  cloud_id: Rails.application.credentials.dig(:elasticsearch, :cloud_id),
+  api_key: Rails.application.credentials.dig(:elasticsearch, :api_key)
+)
 
 module Searchable
   extend ActiveSupport::Concern
@@ -6,5 +12,19 @@ module Searchable
   included do
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
+
+    mapping do
+      indexes :first_name, type: :text
+      indexes :last_name, type: :text
+      indexes :username, type: :text
+      indexes :email, type: :text
+      indexes :phone_no, type: :text
+    end
+  end
+
+
+  def self.search(query)
+    # Search logic goes here ...
+    self.__elasticsearch__.search(query)
   end
 end
